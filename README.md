855247a528348a1263971556d16a4b83ab3be091
Tony <rodrigoantoniocampos13@gmail.com>
2023-10-01 19:34:50 -0600

    sobre mi --creado--

---
+++ README.md
@@ -0,0 +1,3 @@
+
+<h1>Hi! 😁</h1>
+<img src='../tarea/img/instagram_f_icon-icons.com_65485.png' width="100">
--- sections/homework3.ts
+++
@@ -1,9 +0,0 @@
-export
-let numbers = [1, 6,8,-14,2,7,-10,3,5];
-let maxMunber = -999999;
-for (let i = 0; i< numbers.length; i++){
-    if(numbers[i]>= maxMunber){
-        maxMunber = numbers[i]
-    }
-}
-console.log('El número mayor es:', maxMunber);
